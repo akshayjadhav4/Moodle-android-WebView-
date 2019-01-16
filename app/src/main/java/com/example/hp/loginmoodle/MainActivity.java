f22931d1b3ec6e882b4465acc697acd17e4e1129
akshayjadhav4 <akshayjadhav461999@gmail.com>
2019-01-16 21:02:54 +0530

    Andoid app to login collage moodle account.

---
+++ app/src/main/java/com/example/hp/loginmoodle/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.hp.loginmoodle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private WebView webView;
+
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://agiv.edu.in/moodle30/login/index.php");
+    }
+}
